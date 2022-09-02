b566434bdc0b64b42c87db8484d7bb7e7f5cd781
Abril Micaela Rostro Fiorilo <rostrofioriloabril@gmail.com>
2022-09-02 14:23:17 -0300

    cambios avanzando el responsive

---
+++ main.js
@@ -0,0 +1,10 @@
+let span1 = document.querySelector('.inputBox__span1')
+let span2 = document.querySelector('.inputBox__span2')
+let span3 = document.querySelector('.inputBox__span3')
+let span4 = document.querySelector('.inputBox__span4')
+
+//-------------------------------------------------------//
+let input1 = document.querySelector('.inputBox_child1')
+let input2 = document.querySelector('.inputBox_child2')
+let input3 = document.querySelector('.inputBox_child3')
+let input4 = document.querySelector('.inputBox_child4')
